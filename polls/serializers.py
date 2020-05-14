9e62e4685944f2e4da2aa48687169b6dfade3b2b
Startupify.Tech <startupifytech@gmail.com>
2020-05-14 11:40:41 +0000

    new updates

--- polls/serializers.py
+++ polls/serializers.py
@@ -5,5 +5,5 @@ from .models import Category, Question, Choice
 class LazyEncoder(DjangoJSONEncoder):
     def default(self, obj):
         if isinstance(obj, Question):
-            return str(obj.question_text)
+            return str(obj)
         return super().default(obj)
