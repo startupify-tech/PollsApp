ff7d6da7cba5d185deadaae7fae46544427017e3
startupify-tech <startupifytech@gmail.com>
2021-09-02 11:20:33 +0530

    removed cache files

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'b#s*_o(3t3ai_k(c5po@h7a=nj5#vjkd3u7ckhnx@)mi=8fn67'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
+X_FRAME_OPTIONS = '*'
 
 # Application definition
 
@@ -119,5 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-ALLOWED_HOSTS = ['*']
-X_FRAME_OPTIONS = '*'
